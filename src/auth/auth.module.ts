2912705c54d71dc78ae5b340b2e476ece801c2a4
SCAPUTO88 <sandro.caputo@gmail.com>
2023-02-04 21:41:36 -0300

    added decorators, guards, enums and strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt/dist';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}

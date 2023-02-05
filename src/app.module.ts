10c3d3a15fc69c913eff036b9237b8f5a34cd1f7
SCAPUTO88 <sandro.caputo@gmail.com>
2023-02-05 02:27:32 -0300

    added cart modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module'; // 2.1 import the product module
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/store'), // 1.2 Setup the database
-    ProductModule, UserModule, AuthModule, // 2.2 Add the product module
+    ProductModule,
+    UserModule,
+    AuthModule,
+    CartModule, // 2.2 Add the product module
   ],
   controllers: [AppController],
   providers: [AppService],
